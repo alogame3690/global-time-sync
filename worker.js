3889ed61c0a59b6b903d519a5b095137f40562dc
alogame3690 <37336165+alogame3690@users.noreply.github.com>
2025-06-22 19:43:15 +0700

    Create worker.js

---
+++ worker.js
@@ -0,0 +1,20 @@
+export default {
+  async fetch(request, env, ctx) {
+    const now = new Date();
+    const iso = now.toISOString();
+    const timestamp = now.getTime();
+
+    const gmt7 = new Date(now.getTime() + 7 * 60 * 60 * 1000);
+
+    return new Response(
+      JSON.stringify({
+        utc: iso,
+        timestamp,
+        gmt7: gmt7.toISOString()
+      }),
+      {
+        headers: { "content-type": "application/json" },
+      }
+    );
+  },
+};
