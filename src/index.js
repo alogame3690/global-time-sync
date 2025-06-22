ee25af45fac87422387f46f51062798ad8f7cc6c
alogame3690 <37336165+alogame3690@users.noreply.github.com>
2025-06-22 20:21:24 +0700

    Update index.js

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ export default {
     const now = new Date();
     const iso = now.toISOString();
     const timestamp = now.getTime();
-    const gmt7 = new Date(now.getTime() + 7 * 60 * 60 * 1000);
+    const utc7 = new Date(now.getTime() + 7 * 60 * 60 * 1000);
 
     return new Response(
       JSON.stringify({
